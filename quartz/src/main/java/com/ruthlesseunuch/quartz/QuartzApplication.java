abb564145e516caf2086e3314e267e18f14b4fdd
gongxiaodian <2423686910@qq.com>
2019-12-11 11:15:01 +0800

    quartz配置文件

--- quartz/src/main/java/com/ruthlesseunuch/quartz/QuartzApplication.java
+++ quartz/src/main/java/com/ruthlesseunuch/quartz/QuartzApplication.java
@@ -1,9 +1,17 @@
 package com.ruthlesseunuch.quartz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@ServletComponentScan
 @SpringBootApplication
+@EnableTransactionManagement(proxyTargetClass = true)
+@ComponentScan("com.ruthlesseunuch")
+@MapperScan("com.ruthlesseunuch.repository")
 public class QuartzApplication {
 
     public static void main(String[] args) {
---
+++ quartz/src/main/java/com/ruthlesseunuch/quartz/config/SchedulerConfig.java
@@ -0,0 +1,31 @@
+package com.ruthlesseunuch.quartz.config;
+
+import org.quartz.Scheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+/**
+ * @author:zxz
+ * @Description: TODO
+ * @Date: 19-12-11
+ **/
+@Configuration
+public class SchedulerConfig {
+
+    @Autowired
+    private SpringJobFactory springJobFactory;
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setJobFactory(springJobFactory);
+        return schedulerFactoryBean;
+    }
+
+    @Bean
+    public Scheduler scheduler() {
+        return schedulerFactoryBean().getScheduler();
+    }
+}
